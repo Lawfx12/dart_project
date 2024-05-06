98e619d77835b4ba5c7a10eefe8f367b418f0189
Lawfx12 <donskelly12@gmail.com>
2024-05-06 10:43:05 +0100

    first commit

---
+++ hello_world.dart
@@ -0,0 +1,19 @@
+void main() {
+  String message = 'Welcome to my world!';
+  int info = 10;
+  String name = "gabriel"; //this variables stores chinedu
+  String school = 'unizik';
+  String bio = """male
+  female
+  it
+  shemale """;
+  print(message);
+  print(name);
+  print(bio);
+print(school);
+/*
+this my fitst line comment 
+my second line
+my third
+ */
+}
---
+++ playground.dart
@@ -0,0 +1,16 @@
+ import 'dart:io';
+
+main() {
+  stdout.writeln('What is your name: ?');
+  var name = stdin.readLineSync();
+  print('My name is $name');
+
+ 
+
+  // int amount1 = 100;
+  // var amount2 = 200;
+  // print('amount1: $amount1 | amount2: $amount2\n');
+  
+  
+  
+  }
